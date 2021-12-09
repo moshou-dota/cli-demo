f28fd4c63767e35e5ea298226de8271238019ee6
wupeng01 <wupeng01@bilibili.com>
2021-12-09 19:49:02 +0800

    优化输出和文件内容格式化

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # cli-demo
+![cli-demo](https://img.shields.io/badge/cli--demo-v1.0.0-green)
 
 模拟vue-cli的demo
 
@@ -19,10 +20,18 @@ npm start
     - files: 数组里面的字符串填写需要发布到 NPM 的文件名
     - bin: 若项目中包含一些可执行文件，可以把文件名在 bin 字段下
 
-2. ```#!/usr/bin/env node```
+2. ```#!/usr/bin/env node``` -- Shebang
 3. process.cwd() __diranme import.meta.url
 4. npm login -> npm publish -> npm unpublish --force
 
 ## other 
 
 后续想将自定义的ssr用cli包装下
+
+## reference
+
+[你还在重复的搬砖！？写个 cli 工具解放你的双手吧 - 动态生成代码模板](https://juejin.cn/post/6977567286013984776)
+
+[NPM Package.json 字段列表介绍](http://note.youdao.com/noteshare?id=1920a1dbb280471210495b85949b1b8b&sub=2E1FF189DA5944D38FD789E43DDAB422)
+
+[用 Substats 和 Shields.io 为你的个人主页定制动态数据小牌子](https://sspai.com/post/59593)
--- bin/server.js
+++ bin/server.js
@@ -2,30 +2,38 @@
 
 import fs from 'fs'
 import path from 'path'
-import execa from 'execa'
+import execa from 'execa' // 执行命令
+import chalk from 'chalk' // 美化输出
+import prettier from "prettier" // 格式化文件内容格式
+// "node": "^12.17.0 || ^14.13 || >=16.0.0"
 import questions from './question/index.js'
 import { createConfig } from './config.js'
 import createIndexTemplate from './createIndexTemplate.js'
 import createPackageTemplate from './createPackageTempalte.js'
 
-async function init() {
-	function getRootPath() {
+async function init () {
+	function getRootPath () {
 		return path.resolve(process.cwd(), config.packageName)
 	}
 	const answer = await questions()
 	const config = createConfig(answer)
+	const log = console.log;
 
 	// 1. 创建项目文件夹
+	log(chalk.blue(`创建项目文件夹:${config.packageName}`))
 	fs.mkdirSync(getRootPath())
 	// 2. 创建 index.js
-	fs.writeFileSync(`${getRootPath()}/index.js`, createIndexTemplate(config))
+	log(chalk.blue(`创建入口文件：index.js`))
+	fs.writeFileSync(`${getRootPath()}/index.js`, prettier.format(createIndexTemplate(config), { parser: "babel" }))
 	// 3. 创建 package.json
+	log(chalk.blue(`创建 package.json`))
 	fs.writeFileSync(
 		`${getRootPath()}/package.json`,
-		createPackageTemplate(config)
+		prettier.format(createPackageTemplate(config), { parser: "json" })
 	)
 
 	// 4. 安装依赖
+	log(chalk.blue(`安装依赖`))
 	// execa('yarn', [], {
 	// 	cwd: getRootPath()
 	// }).stdout.pipe(process.stdout)
